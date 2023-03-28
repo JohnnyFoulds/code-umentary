22efcf2195d769cf110f849bffe661e1e97137c7
Johannes Foulds <johannes.foulds@vodacom.co.za>
2023-03-28 16:34:17 +0200

    Try to, not much better

--- src/app.js
+++ src/app.js
@@ -10,27 +10,13 @@ $(document).ready(function() {
 		// Append the new content block to the main content area
 		$('main').append('<div class="contentBlock" id="' + blockID + '"><p>Block ' + blockCount + '</p><button class="editButton">Edit</button></div>');
 	});
-});
-
-$(document).ready(function() {
-	// Generate a unique ID for each new content block
-	let blockCount = 0;
-	
-	// Handle click event on "New Block" button
-	$('#newBlockButton').click(function() {
-		blockCount++;
-		// Create a new content block with unique ID
-		let blockID = 'block' + blockCount;
-		// Append the new content block to the main content area
-		$('main').append('<div class="contentBlock" id="' + blockID + '"><p>Block ' + blockCount + '</p><button class="editButton">Edit</button></div>');
-	});
 	
 	// Handle click event on "Edit" button within a content block
 	$('main').on('click', '.editButton', function() {
 		// Get the ID of the content block being edited
 		let blockID = $(this).parent().attr('id');
-		// Get the Markdown content of the content block
-		let blockContent = $('#' + blockID).html();
+		// Get the original HTML content of the content block
+		let blockContent = $('#' + blockID).data('originalContent') || $('#' + blockID).html();
 		// Replace the content block with a textarea containing the Markdown content
 		$('#' + blockID).html('<textarea class="editArea">' + blockContent + '</textarea><button class="saveButton">Save</button>');
 	});
@@ -46,5 +32,7 @@ $(document).ready(function() {
 		let htmlContent = converter.makeHtml(blockContent);
 		// Replace the textarea with the new HTML content
 		$('#' + blockID).html(htmlContent);
-    });
-})
+		// Store the new HTML content as the original content of the content block
+		$('#' + blockID).data('originalContent', htmlContent);
+	});
+});
